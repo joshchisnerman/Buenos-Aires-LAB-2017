37d0928ae1ded82e705cc9173483d08005d1f896
baprovincialab1 <baprovincialab@gmail.com>
2017-12-14 16:28:54 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2817,6 +2817,7 @@ END Page Load
             });
         });
 
+
         [].slice.call(document.querySelectorAll('.link-copy')).forEach(function(link) {
             link.setAttribute('data-clipboard-text', location.protocol + '//' + location.host + location.pathname + '#' + link.parentNode.id);
             new Clipboard(link);
@@ -2829,6 +2830,41 @@ END Page Load
         });
     }
 
+   
+
     init();
 
 })(window);
+
+/* to FIX
+
+
+jQuery(document).ready(function($){
+
+ var contentSections = $('.full-screen'),
+        navigationItems = $('#cd-vertical-nav a');
+
+
+    updateNavigation();
+
+    $(window).on('scroll', function(){
+        updateNavigation();
+    });
+
+
+        function updateNavigation() {
+        contentSections.each(function(){
+            $this = $(this);
+            var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+            if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+                navigationItems.eq(activeSection).addClass('nav__item--current');
+            }else {
+                navigationItems.eq(activeSection).removeClass('nav__item--current');
+            }
+        });
+    }
+
+
+    }
+
+    */
